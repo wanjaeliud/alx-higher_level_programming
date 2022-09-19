ededc0ef3c111e2a63e4d6e227a1e2bd445860a2
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-20 01:31:43 +0300

    concatenate string && git push

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+    language that combines remarkable power with very clear syntax"
+str1 = str[39:67] + str[-22:-17] + str[:6]
+print(str1)
